239678bedd0667b3ce7a719068ee8311fbc4a7e2
tiot07 <tiot7155@gmail.com>
2019-01-05 04:44:37 +0900

    cookie

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const http=require('http');
 const server = http.createServer((req,res)=>{
+  const now = Date.now();
+  res.setHeader('Set-Cookie','last_access='+now+';');
   res.end('hi');
 });
 const port=8000;
